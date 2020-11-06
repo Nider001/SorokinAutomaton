dbe07ce15775b1a843b015dd8eb3b39fd784402d
Nider001 <222113222113z@gmail.com>
2020-11-06 14:34:20 +0400

    first commit

---
+++ Automaton1/Automaton.cs
@@ -0,0 +1,175 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Automaton1
+{
+    class Pair<T1, T2>
+    {
+        public T1 Key { get; set; }
+        public T2 Value { get; set; }
+
+        public Pair(T1 key, T2 value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Pair<T1, T2> pair &&
+                   EqualityComparer<T1>.Default.Equals(Key, pair.Key) &&
+                   EqualityComparer<T2>.Default.Equals(Value, pair.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 206514262;
+            hashCode = hashCode * -1521134295 + EqualityComparer<T1>.Default.GetHashCode(Key);
+            hashCode = hashCode * -1521134295 + EqualityComparer<T2>.Default.GetHashCode(Value);
+            return hashCode;
+        }
+    }
+
+    class AutomatonData
+    {
+        public bool UseRegex { get; set; }
+        public KeyValuePair<Pair<string, string>, string>[] Transitions { get; set; }
+        public HashSet<string> Start { get; set; }
+        public HashSet<string> Finish { get; set; }
+
+        public AutomatonData()
+        {
+            UseRegex = false;
+            Transitions = new KeyValuePair<Pair<string, string>, string>[0];
+            Start = new HashSet<string>();
+            Finish = new HashSet<string>();
+        }
+
+        public AutomatonData(bool useRegex, Dictionary<Pair<string, string>, string> transitions, HashSet<string> start, HashSet<string> finish) : this(useRegex, transitions.ToArray(), start, finish)
+        {
+
+        }
+
+        public AutomatonData(bool useRegex, KeyValuePair<Pair<string, string>, string>[] transitions, HashSet<string> start, HashSet<string> finish)
+        {
+            UseRegex = useRegex;
+            Transitions = transitions ?? throw new ArgumentNullException(nameof(transitions));
+            Start = start ?? throw new ArgumentNullException(nameof(start));
+            Finish = finish ?? throw new ArgumentNullException(nameof(finish));
+        }
+    }
+
+    class Automaton
+    {
+        bool useRegex;
+        KeyValuePair<Pair<string, string>, string>[] transitions; //<<q1, '...'>, q2>
+        HashSet<string> start;
+        HashSet<string> finish;
+
+        public Automaton(bool useRegex, KeyValuePair<Pair<string, string>, string>[] transitions, HashSet<string> start, HashSet<string> finish)
+        {
+            this.useRegex = useRegex;
+            this.transitions = transitions ?? throw new ArgumentNullException(nameof(transitions));
+            this.start = start ?? throw new ArgumentNullException(nameof(start));
+            this.finish = finish ?? throw new ArgumentNullException(nameof(finish));
+        }
+
+        public Automaton(bool useRegex, Dictionary<Pair<string, string>, string> transitions, HashSet<string> start, HashSet<string> finish) : this(useRegex, transitions.ToArray(), start, finish)
+        {
+
+        }
+
+        public Automaton(AutomatonData data) : this(data.UseRegex, data.Transitions, data.Start, data.Finish)
+        {
+
+        }
+
+        private void MoveState(string symbol, ref HashSet<string> currentStates, ref Pair<bool, int> result, ref bool irrivedToFinal)
+        {
+            try
+            {
+                bool countIncreasedThisTime = false;
+
+                var tempStates = currentStates;
+                currentStates = new HashSet<string>();
+
+                bool passed = false;
+
+                foreach (string cs in tempStates)
+                {
+                    var potentialTransitions = useRegex ? transitions.Where(x => x.Key.Key == cs && Regex.IsMatch(symbol, x.Key.Value)).ToArray() : transitions.Where(x => x.Key.Key == cs && symbol == x.Key.Value).ToArray();
+                    if (!passed && potentialTransitions.Length != 0)
+                    {
+                        passed = true;
+                    }
+
+                    foreach (var temp in potentialTransitions)
+                    {
+                        if (!irrivedToFinal && finish.Contains(temp.Value))
+                        {
+                            irrivedToFinal = true;
+                        }
+
+                        if (!currentStates.Contains(temp.Value))
+                        {
+                            currentStates.Add(temp.Value);
+
+                            if (!countIncreasedThisTime && result.Key == true)
+                            {
+                                countIncreasedThisTime = true;
+                                result.Value++;
+                            }
+                        }
+                    }
+                }
+
+                if (!passed)
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                result.Key = false;
+            }
+        }
+
+        public Pair<bool, int> Max(string data, int skip)
+        {
+            if (skip < 0 || skip >= data.Length)
+            {
+                throw new ArgumentException(nameof(skip));
+            }
+
+            HashSet<string> currentStates = new HashSet<string>(start);
+            Pair<bool, int> result = new Pair<bool, int>(true, 0);
+
+            char[] array = data.ToCharArray().Skip(skip).ToArray();
+
+            bool irrivedToFinal = currentStates.Any(x => finish.Contains(x)) ? true : false;
+
+            foreach (char item in array)
+            {
+                MoveState(item.ToString(), ref currentStates, ref result, ref irrivedToFinal);
+
+                if (!result.Key)
+                {
+                    break;
+                }
+            }
+
+            if (!irrivedToFinal)
+            {
+                result.Key = false;
+                result.Value = 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Automaton1/Program.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Automaton1
+{
+    class Program
+    {
+        static Pair<bool, int> Execute(Automaton automaton, string str, int skip)
+        {
+            return automaton.Max(str, skip);
+        }
+
+        static void Main(string[] args)
+        {
+            Automaton special = null;
+            Automaton keyWord = null;
+            Automaton constBool = null;
+            Automaton constDouble = null;
+            Automaton constInt = null;
+            Automaton id = null;
+            Automaton assign = null;
+            Automaton oper = null;
+            Automaton bracket = null;
+
+            using (StreamReader sr = new StreamReader("special.txt"))
+            {
+                string t = sr.ReadToEnd();
+                special = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("keyword.txt"))
+            {
+                string t = sr.ReadToEnd();
+                keyWord = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("const_bool.txt"))
+            {
+                string t = sr.ReadToEnd();
+                constBool = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("const_double.txt"))
+            {
+                string t = sr.ReadToEnd();
+                constDouble = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("const_int.txt"))
+            {
+                string t = sr.ReadToEnd();
+                constInt = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("id.txt"))
+            {
+                string t = sr.ReadToEnd();
+                id = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("assign.txt"))
+            {
+                string t = sr.ReadToEnd();
+                assign = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("oper.txt"))
+            {
+                string t = sr.ReadToEnd();
+                oper = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+            using (StreamReader sr = new StreamReader("bracket.txt"))
+            {
+                string t = sr.ReadToEnd();
+                bracket = new Automaton(JsonConvert.DeserializeObject<AutomatonData>(t));
+            }
+
+
+            //=================================================================================
+
+            string code = "";
+            Pair<bool, int> state = new Pair<bool, int>(false, 0);
+            int iter = 0;
+
+            List<Pair<string, string>> tokens = new List<Pair<string, string>>();
+
+            using (StreamReader sr = new StreamReader("CODE.txt"))
+            {
+                code = Regex.Replace(sr.ReadToEnd(), @"\s", "");
+            }
+
+            Dictionary<Automaton, string> automatons = new Dictionary<Automaton, string>();
+            automatons.Add(special, "special");
+            automatons.Add(keyWord, "keyWord");
+            automatons.Add(constBool, "constBool");
+            automatons.Add(constDouble, "constDouble");
+            automatons.Add(constInt, "constInt");
+            automatons.Add(id, "id");
+            automatons.Add(assign, "assign");
+            automatons.Add(oper, "oper");
+            automatons.Add(bracket, "bracket");
+
+            while (iter < code.Length)
+            {
+
+                foreach (var automaton in automatons)
+                {
+                    state = Execute(automaton.Key, code, iter);
+
+                    if (state.Value != 0)
+                    {
+                        tokens.Add(new Pair<string, string>(automaton.Value, code.Substring(iter, state.Value)));
+                        iter += state.Value;
+                        break;
+                    }
+                    else if (automaton.Value == automatons.Values.Last())
+                    {
+                        throw new ArgumentException("Error: invalid code at '" + code[iter] + "...'");
+                    }
+                }
+
+                string json = JsonConvert.SerializeObject(tokens, Formatting.Indented);
+
+                using (StreamWriter sr = new StreamWriter("RESULT.txt"))
+                {
+                    sr.Write(json);
+                }
+            }
+        }
+    }
+}
